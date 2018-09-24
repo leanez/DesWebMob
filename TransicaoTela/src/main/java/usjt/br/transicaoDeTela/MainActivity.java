104c89731279b11b7a4dae5520afe5b379915d4a
leanez <leila.anez@ymail.com>
2018-09-24 18:58:09 -0300

    Transição de Tela

---
+++ TransicaoTela/src/main/java/usjt/br/transicaoDeTela/MainActivity.java
@@ -0,0 +1,31 @@
+/*
+Nome: Leila Añez de Oliveira
+RA: 81618710
+*/
+package usjt.br.transicaoDeTela;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+    public final  static String  mensagem= "br.usjt.transicaoDeTela.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(mensagem, message);
+        startActivity(intent);
+
+    }
+}
