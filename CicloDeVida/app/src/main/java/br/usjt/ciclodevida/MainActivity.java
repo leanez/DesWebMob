ddbe65cf925aa7e1a9dd08882bbcba1a4ef03673
leanez <leila.anez@ymail.com>
2018-09-24 19:06:50 -0300

    Ciclo de Vida

---
+++ CicloDeVida/app/src/main/java/br/usjt/ciclodevida/MainActivity.java
@@ -0,0 +1,19 @@
+/*
+Nome: Leila Añez de Oliveira
+RA: 81618710
+ */
+
+package br.usjt.ciclodevida;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
